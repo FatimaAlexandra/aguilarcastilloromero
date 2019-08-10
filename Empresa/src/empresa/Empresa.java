3736924f6dae4be89085d74c794f038a8b669f3d
Fatima <fathykztillo@gmail.com>
2019-08-09 23:08:51 -0600

    proyecto

--- Empresa/src/empresa/Empresa.java
+++ Empresa/src/empresa/Empresa.java
@@ -19,8 +19,8 @@ public class Empresa {
         System.out.println("Escriban aqui hola xd");
         
         JOptionPane.showMessageDialog(null, "probando :v ");
-        
-        
+         JOptionPane.showMessageDialog(null, "probando :v ");
+         JOptionPane.showMessageDialog(null, "probando :v ");
         
         
         JOptionPane.showMessageDialog(null, "si puedo ver tu probando");
