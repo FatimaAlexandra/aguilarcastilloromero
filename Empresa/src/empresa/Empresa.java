36d8367a3d9328c9da33b80846963423c357f7ba
Fatima <fathykztillo@gmail.com>
2019-08-09 22:30:43 -0600

    cambios2

--- Empresa/src/empresa/Empresa.java
+++ Empresa/src/empresa/Empresa.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package empresa;
 
 /**
  *
- * @author Alexandra
+ * @author SIS13B
  */
 public class Empresa {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola a todos c: ");
